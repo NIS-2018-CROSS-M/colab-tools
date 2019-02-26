87ca58fb2d4696d522c5c66b93e0f690c1c38575
oserikov <srkvoa@gmail.com>
2019-02-26 20:55:02 +0300

    fixed conda install

--- colab-install-anaconda.sh
+++ colab-install-anaconda.sh
@@ -1,1 +1,2 @@
-bash <(curl -s https://repo.continuum.io/archive/Anaconda3-5.1.0-Linux-x86_64.sh)
+wget -q -c https://repo.continuum.io/archive/Anaconda3-5.1.0-Linux-x86_64.sh -O conda_installer.sh
+bash conda_installer.sh -b -f -p /usr/local
