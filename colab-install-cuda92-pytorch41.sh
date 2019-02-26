f14a7d63279b7bf050b213dec6500977c778924a
oserikov <srkvoa@gmail.com>
2019-02-26 20:57:29 +0300

    fixed cuda install

--- colab-install-cuda92-pytorch41.sh
+++ colab-install-cuda92-pytorch41.sh
@@ -1,1 +1,2 @@
-bash <(https://gist.githubusercontent.com/oserikov/fe02f3094b5feb4e46f042bcf338431e/raw/174b34c058cc1f71e01515fc05b6db8f25d16ab0/pytorch041_cuda92_colab.sh)
+wget -q -c https://gist.githubusercontent.com/oserikov/fe02f3094b5feb4e46f042bcf338431e/raw/174b34c058cc1f71e01515fc05b6db8f25d16ab0/pytorch041_cuda92_colab.sh -O cuda_torch_installer.sh
+bash cuda_torch_installer.sh
